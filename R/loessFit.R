45f1afb731a79e303fc9e19c5b8f9c9d5c583f5e
Gordon Smyth <smyth@wehi.edu.au>
2012-09-06 04:08:40 +0000

    6 Sep 2012: limma 3.12.3
    - loessFit() now applies minimum and maximum bounds to avoid zero or
      infinite weights.  Equal weights are now treated as NULL, even all
      zero weights, so that lowess is called instead of loess.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_10/madman/Rpacks/limma@69128 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/loessFit.R
+++ R/loessFit.R
@@ -1,12 +1,12 @@
 #	LOESS FUNCTIONS
 
-loessFit <- function(y, x, weights=NULL, span=0.3, bin=NULL, iterations=4) {
+loessFit <- function(y, x, weights=NULL, span=0.3, bin=NULL, iterations=4, min.weight=1e-5, max.weight=1e5, equal.weights.as.null=TRUE)
 #	Fast loess fit for simple x and y
 #	This function uses stats:::lowess if no weights and stats:::loess otherwise.
 #	It is intended to give a streamlined common interface to the two functions.
 #	Gordon Smyth
-#	28 June 2003.  Last revised 10 May 2012.
-
+#	28 June 2003.  Last revised 5 Sep 2012.
+{
 	n <- length(y)
 	out <- list(fitted=rep(NA,n),residuals=rep(NA,n))
 	obs <- is.finite(y) & is.finite(x)
@@ -27,17 +27,34 @@ loessFit <- function(y, x, weights=NULL, span=0.3, bin=NULL, iterations=4) {
 		out$residuals[obs][o] <- lo[[9]]
 	} else {
 		if(is.null(bin)) bin <- 0.005
-		weights[!is.finite(weights)] <- 0
 		wobs <- weights[obs]
-		if(sum(wobs>0) < 4+1/span) {
-			fit <- lm.wfit(cbind(1,xobs),yobs,wobs)
+		wobs[is.na(wobs)] <- 0
+		wobs <- pmax(wobs,min.weight)
+		wobs <- pmin(wobs,max.weight)
+#		Test whether weights are equal
+		if(equal.weights.as.null) {
+			r <- range(wobs)
+			if(r[2]-r[1] < 1e-15) return(Recall(y,x,span=span,bin=bin,iterations=iterations))
+		}
+#		Count number of observations with positive weights
+		if(min.weight>0)
+			nwobs <- nobs
+		else 
+			nwobs <- sum(wobs>0)
+		if(nwobs < 4+1/span) {
+			if(nwobs>1) {
+				fit <- lm.wfit(cbind(1,xobs),yobs,wobs)
+			} else {
+				fit <- list()
+				fit$fitted <- rep(sum(wobs*yobs)/sum(wobs),nobs)
+				fit$residuals <- yobs-fit$fitted
+			}
 		} else {
 #			Suppress warning "k-d tree limited by memory"
 #			oldopt <- options(warning.expression=expression())
 			oldopt <- options(warn=-1)
 			on.exit(options(oldopt))
-			fit <- .vsimpleLoess(y=yobs, x=xobs, weights=wobs, span=span, degree=1,
-				cell=bin/span, iterations=iterations)
+			fit <- .vsimpleLoess(y=yobs, x=xobs, weights=wobs, span=span, degree=1,	cell=bin/span, iterations=iterations)
 		}
 		out$fitted[obs] <- fit$fitted
 		out$residuals[obs] <- fit$residuals
