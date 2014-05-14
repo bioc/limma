6e21a164c7ffc0e35265e4fe4b6c1b029e6f6535
Gordon Smyth <smyth@wehi.edu.au>
2014-05-14 07:49:13 +0000

    13 May 2014: limma 3.20.2
    - Subsetting columns of a MArrayLM object should not subset the
      design matrix.  Now fixed.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_2_14/madman/Rpacks/limma@90309 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/subsetting.R
+++ R/subsetting.R
@@ -102,12 +102,12 @@ assign("[.MArrayLM",
 function(object, i, j)
 #  Subsetting for MArrayLM objects
 #  Gordon Smyth
-#  26 April 2005. Last modified 11 Feb 2014.
+#  26 April 2005. Last modified 14 May 2014.
 {
 #	Recognized components
 	IJ <- c("coefficients","stdev.unscaled","t","p.value","lods","weights")
 	IX <- "genes"
-	JX <- c("targets","design")
+	JX <- character(0)
 	I  <- c("Amean","sigma","df.residual","df.prior","df.total","s2.post","F","F.p.value")
 	JJ <- "cov.coefficients"
 	XJ <- "contrasts"
