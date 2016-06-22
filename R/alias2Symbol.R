419894a05828ebfa67d67eef98e433024a5c26b3
Gordon Smyth <smyth@wehi.edu.au>
2016-06-22 08:55:34 +0000

    22 Jun 2016: limma 3.29.11
    - alias2Symbol() and alias2SymbolTable() now for for any species for
      which an Entrez Gene based organism package exists.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@118838 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/alias2Symbol.R
+++ R/alias2Symbol.R
@@ -4,49 +4,38 @@ alias2Symbol <- function(alias,species="Hs",expand.symbols=FALSE)
 #  Convert a set of alias names to official gene symbols
 #  via Entrez Gene identifiers
 #  Di Wu, Gordon Smyth and Yifang Hu
-#  4 Sep 2008. Last revised 2 May 2016.
+#  4 Sep 2008. Last revised 22 June 2016.
 {
 	alias <- as.character(alias)
-	species <- match.arg(species,c("Dm","Hs","Mm","Rn"))
 
 #	Get access to required annotation functions
 	suppressPackageStartupMessages(OK <- requireNamespace("AnnotationDbi",quietly=TRUE))
 	if(!OK) stop("AnnotationDbi package required but not installed (or can't be loaded)")
 
-#	Get alias to symbol mappings
-	switch(species,
-		Hs = {
-			suppressPackageStartupMessages(OK <- requireNamespace("org.Hs.eg.db",quietly=TRUE))
-			if(!OK) stop("org.Hs.eg.db package required but not installed (or can't be loaded)")
-			ALIAS2EG <- org.Hs.eg.db::org.Hs.egALIAS2EG
-			SYMBOL <- org.Hs.eg.db::org.Hs.egSYMBOL
-		}, Mm = {
-			suppressPackageStartupMessages(OK <- requireNamespace("org.Mm.eg.db",quietly=TRUE))
-			if(!OK) stop("org.Mm.eg.db package required but not installed (or can't be loaded)")
-			ALIAS2EG <- org.Mm.eg.db::org.Mm.egALIAS2EG
-			SYMBOL <- org.Mm.eg.db::org.Mm.egSYMBOL
-		}, Rn = {
-			suppressPackageStartupMessages(OK <- requireNamespace("org.Rn.eg.db",quietly=TRUE))
-			if(!OK) stop("org.Rn.eg.db package required but not installed (or can't be loaded)")
-			ALIAS2EG <- org.Rn.eg.db::org.Rn.egALIAS2EG
-			SYMBOL <- org.Rn.eg.db::org.Rn.egSYMBOL
-		}, Dm = {
-			suppressPackageStartupMessages(OK <- requireNamespace("org.Dm.eg.db",quietly=TRUE))
-			if(!OK) stop("org.Dm.eg.db package required but not installed (or can't be loaded)")
-			ALIAS2EG <- org.Dm.eg.db::org.Dm.egALIAS2EG
-			SYMBOL <- org.Dm.eg.db::org.Dm.egSYMBOL
-		}
-	)
+#	Load appropriate organism package
+	orgPkg <- paste0("org.",species,".eg.db")
+	suppressPackageStartupMessages(OK <- requireNamespace(orgPkg,quietly=TRUE))
+	if(!OK) stop(orgPkg," package required but not not installed (or can't be loaded)")
+
+#	Get alias to Entrez Gene mappings
+	obj <- paste0("org.",species,".egALIAS2EG")
+	egALIAS2EG <- tryCatch(getFromNamespace(obj,orgPkg), error=function(e) FALSE)
+	if(is.logical(egALIAS2EG)) stop("Couldn't get ",paste(orgPkg,obj,sep="::"))
+
+#	Get symbol to Entrez Gene mappings
+	obj <- paste0("org.",species,".egSYMBOL")
+	egSYMBOL <- tryCatch(getFromNamespace(obj,orgPkg), error=function(e) FALSE)
+	if(is.logical(egSYMBOL)) stop("Couldn't get ",paste(orgPkg,obj,sep="::"))
 
 	if(expand.symbols) {
-		alias <- intersect(alias,AnnotationDbi::Rkeys(ALIAS2EG))
-		eg <- AnnotationDbi::mappedLkeys(ALIAS2EG[alias])
-		AnnotationDbi::mappedRkeys(SYMBOL[eg])
+		alias <- intersect(alias,AnnotationDbi::Rkeys(egALIAS2EG))
+		eg <- AnnotationDbi::mappedLkeys(egALIAS2EG[alias])
+		AnnotationDbi::mappedRkeys(egSYMBOL[eg])
 	} else {
-		isSymbol <- alias %in% AnnotationDbi::Rkeys(SYMBOL) 
-		alias2 <- intersect(alias[!isSymbol],AnnotationDbi::Rkeys(ALIAS2EG))
-		eg <- AnnotationDbi::mappedLkeys(ALIAS2EG[alias2])
-		c(alias[isSymbol],AnnotationDbi::mappedRkeys(SYMBOL[eg]))
+		isSymbol <- alias %in% AnnotationDbi::Rkeys(egSYMBOL) 
+		alias2 <- intersect(alias[!isSymbol],AnnotationDbi::Rkeys(egALIAS2EG))
+		eg <- AnnotationDbi::mappedLkeys(egALIAS2EG[alias2])
+		c(alias[isSymbol],AnnotationDbi::mappedRkeys(egSYMBOL[eg]))
 	}
 }
 
@@ -54,55 +43,48 @@ alias2SymbolTable <- function(alias,species="Hs")
 #  Convert a vector of alias names to the vector of corresponding official gene symbols
 #  via Entrez Gene identifiers
 #  Di Wu, Gordon Smyth and Yifang Hu
-#  Created 3 Sep 2009.  Last modified 13 April 2016.
+#  Created 3 Sep 2009.  Last modified 22 April 2016.
 {
 	alias <- as.character(alias)
-	species <- match.arg(species,c("Dm","Hs","Mm","Rn"))
 
 #	Get access to required annotation functions
 	suppressPackageStartupMessages(OK <- requireNamespace("AnnotationDbi",quietly=TRUE))
 	if(!OK) stop("AnnotationDbi package required but not installed (or can't be loaded)")
 
-#	Get alias to symbol mappings
-	switch(species,
-		Hs = {
-			suppressPackageStartupMessages(OK <- requireNamespace("org.Hs.eg.db",quietly=TRUE))
-			if(!OK) stop("org.Hs.eg.db package required but not installed (or can't be loaded)")
-			ALIAS2EG <- org.Hs.eg.db::org.Hs.egALIAS2EG
-			SYMBOL <- org.Hs.eg.db::org.Hs.egSYMBOL
-		}, Mm = {
-			suppressPackageStartupMessages(OK <- requireNamespace("org.Mm.eg.db",quietly=TRUE))
-			if(!OK) stop("org.Mm.eg.db package required but not installed (or can't be loaded)")
-			ALIAS2EG <- org.Mm.eg.db::org.Mm.egALIAS2EG
-			SYMBOL <- org.Mm.eg.db::org.Mm.egSYMBOL
-		}, Rn = {
-			suppressPackageStartupMessages(OK <- requireNamespace("org.Rn.eg.db",quietly=TRUE))
-			if(!OK) stop("org.Rn.eg.db package required but not installed (or can't be loaded)")
-			ALIAS2EG <- org.Rn.eg.db::org.Rn.egALIAS2EG
-			SYMBOL <- org.Rn.eg.db::org.Rn.egSYMBOL
-		}, Dm = {
-			suppressPackageStartupMessages(OK <- requireNamespace("org.Dm.eg.db",quietly=TRUE))
-			if(!OK) stop("org.Dm.eg.db package required but not installed (or can't be loaded)")
-			ALIAS2EG <- org.Dm.eg.db::org.Dm.egALIAS2EG
-			SYMBOL <- org.Dm.eg.db::org.Dm.egSYMBOL
-		}
-	)
+#	Load appropriate organism package
+	orgPkg <- paste0("org.",species,".eg.db")
+	suppressPackageStartupMessages(OK <- requireNamespace(orgPkg,quietly=TRUE))
+	if(!OK) stop(orgPkg," package required but not not installed (or can't be loaded)")
 
-	isSymbol <- alias %in% AnnotationDbi::Rkeys(SYMBOL)
+#	Get alias to Entrez Gene mappings
+	obj <- paste0("org.",species,".egALIAS2EG")
+	egALIAS2EG <- tryCatch(getFromNamespace(obj,orgPkg), error=function(e) FALSE)
+	if(is.logical(egALIAS2EG)) stop("Couldn't get ",paste(orgPkg,obj,sep="::"))
+
+#	Get symbol to Entrez Gene mappings
+	obj <- paste0("org.",species,".egSYMBOL")
+	egSYMBOL <- tryCatch(getFromNamespace(obj,orgPkg), error=function(e) FALSE)
+	if(is.logical(egSYMBOL)) stop("Couldn't get ",paste(orgPkg,obj,sep="::"))
+
+#	Output vector same length as input
 	Symbol <- alias
+
+#	Check which aliases are already symbols
+	isSymbol <- alias %in% AnnotationDbi::Rkeys(egSYMBOL)
 	Symbol[!isSymbol] <- NA
 
+#	Now deal with rest
 	OtherAliases <- alias[!isSymbol]
-	isAlias <- OtherAliases %in% AnnotationDbi::Rkeys(ALIAS2EG)
+	isAlias <- OtherAliases %in% AnnotationDbi::Rkeys(egALIAS2EG)
 	if(!any(isAlias)) return(Symbol)
 	OtherAliases <- OtherAliases[isAlias]
-
-	AliasTbl <- AnnotationDbi::toTable(ALIAS2EG[OtherAliases])
+	AliasTbl <- AnnotationDbi::toTable(egALIAS2EG[OtherAliases])
 	if(anyDuplicated(AliasTbl$alias_symbol)) warning("Multiple symbols ignored for one or more aliases")
-	SymbolTbl <- AnnotationDbi::toTable(SYMBOL[AliasTbl$gene_id])
+	SymbolTbl <- AnnotationDbi::toTable(egSYMBOL[AliasTbl$gene_id])
 	m <- match(OtherAliases,AliasTbl$alias_symbol)
 	GeneID <- AliasTbl$gene_id[m]
 	m <- match(GeneID,SymbolTbl$gene_id)
 	Symbol[!isSymbol][isAlias] <- SymbolTbl$symbol[m]
+
 	Symbol
 }
