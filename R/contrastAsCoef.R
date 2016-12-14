f86093930401a8320af528f991c377c4fb554ef6
Gordon Smyth <smyth@wehi.edu.au>
2016-12-14 03:50:58 +0000

    14 Dec 2016: limma 3.31.7
    - Bug fix for contrastAsCoef() when there is more than one contrast.
      Previously the coefficients for the transformed design matrix were
      correct only for the first contrast.
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/limma@125104 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/contrastAsCoef.R
+++ R/contrastAsCoef.R
@@ -1,26 +1,27 @@
 contrastAsCoef <- function(design, contrast=NULL, first=TRUE)
 #	Reform a design matrix so that one or more contrasts become simple coefficients
 #	Gordon Smyth
-#	31 August 2013
+#	31 August 2013. Last modified 14 Dec 2016.
 {
 	design <- as.matrix(design)
 	if(is.null(contrast)) return(design)
 	contrast <- as.matrix(contrast)
 	if(ncol(design) != nrow(contrast)) stop("Length of contrast doesn't match ncol(design)")
-	qrc <- qr(contrast)
-	ncontrasts <- qrc$rank
+	QRc <- qr(contrast)
+	ncontrasts <- QRc$rank
 	if(ncontrasts==0) stop("contrast is all zero")
 	coef <- 1:ncontrasts
-	Dvec <- diag(qrc$qr)[coef]
-	design <- t(qr.qty(qrc,t(design)))
+	designT <- qr.qty(QRc,t(design))
+	R <- QRc$qr[coef,]
+	designT[coef,] <- backsolve(R,designT[coef,,drop=FALSE])
+	design <- t(designT)
 	colnames(design) <- paste("Q",1:ncol(design),sep="")
 	cn <- colnames(contrast)
-	if(is.null(cn)) cn <- paste("C",qrc$pivot[coef],sep="")
+	if(is.null(cn)) cn <- paste("C",QRc$pivot[coef],sep="")
 	colnames(design)[coef] <- cn
 	if(!first) {
 		design <- cbind(design[,-coef,drop=FALSE],design[,coef,drop=FALSE])
 		coef <- rev( ncol(design)-coef+1 )
 	}
-	design[,coef] <- t(t(design[,coef])/Dvec)
-	list(design=design,coef=coef,qr=qrc)
+	list(design=design,coef=coef,qr=QRc)
 }
