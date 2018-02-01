c94665a269d8341c648a8be1370f2e5ebeacf159
Gordon Smyth <smyth@wehi.edu.au>
2018-02-01 19:54:57 +1100

    1 Feb 2018: limma 3.34.7
    - toptable() is now officially deprecated in favor of topTable().

--- R/toptable.R
+++ R/toptable.R
@@ -3,7 +3,7 @@
 topTable <- function(fit,coef=NULL,number=10,genelist=fit$genes,adjust.method="BH",sort.by="B",resort.by=NULL,p.value=1,lfc=0,confint=FALSE)
 #	Summary table of top genes, object-orientated version
 #	Gordon Smyth
-#	4 August 2003.  Last modified 10 April 2015.
+#	4 August 2003.  Last modified 1 Feb 2018.
 {
 #	Check fit
 	if(!is(fit,"MArrayLM")) stop("fit must be an MArrayLM object")
@@ -35,7 +35,7 @@ topTable <- function(fit,coef=NULL,number=10,genelist=fit$genes,adjust.method="B
 	fit <- unclass(fit)
 	ebcols <- c("t","p.value","lods")
 	if(confint) ebcols <- c("s2.post","df.total",ebcols)
-	toptable(fit=fit[c("coefficients","stdev.unscaled")],
+	.topTableT(fit=fit[c("coefficients","stdev.unscaled")],
 		coef=coef,
 		number=number,
 		genelist=genelist,
@@ -132,10 +132,10 @@ topTableF <- function(fit,number=10,genelist=fit$genes,adjust.method="BH",sort.b
 	tab
 }
 
-toptable <- function(fit,coef=1,number=10,genelist=NULL,A=NULL,eb=NULL,adjust.method="BH",sort.by="B",resort.by=NULL,p.value=1,lfc=0,confint=FALSE,...)
-#	Summary table of top genes
+.topTableT <- function(fit,coef=1,number=10,genelist=NULL,A=NULL,eb=NULL,adjust.method="BH",sort.by="B",resort.by=NULL,p.value=1,lfc=0,confint=FALSE,...)
+#	Summary table of top genes for a single coefficient
 #	Gordon Smyth
-#	21 Nov 2002. Last revised 2 March 2017.
+#	21 Nov 2002. Forked from toptable() 1 Feb 2018. Last revised 1 Feb 2018.
 {
 #	Check fit
 	fit$coefficients <- as.matrix(fit$coefficients)
@@ -188,18 +188,182 @@ toptable <- function(fit,coef=1,number=10,genelist=NULL,A=NULL,eb=NULL,adjust.me
 		if(NCOL(A)>1) A <- rowMeans(A,na.rm=TRUE)
 	}
 
-#	Compute eb if not given, compute just the one column required
+#	If eb is NULL, look for components in fit
 	if(is.null(eb)) {
-		fit$coefficients <- fit$coefficients[,coef,drop=FALSE]
-		fit$stdev.unscaled <- as.matrix(fit$stdev.unscaled)[,coef,drop=FALSE]
-		eb <- ebayes(fit,...)
-		coef <- 1
+		ebcols <- c("t","p.value","lods")
+		if(confint) ebcols <- c("s2.post","df.total",ebcols)
+		eb <- fit[ebcols]
+#		If still NULL, compute using ebayes just for the one column required
+		if(is.null(eb)) {
+			message("Moderated t-statistics and p-values not found in fit, running ebayes() to get them.")
+			fit$coefficients <- fit$coefficients[,coef,drop=FALSE]
+			fit$stdev.unscaled <- as.matrix(fit$stdev.unscaled)[,coef,drop=FALSE]
+			eb <- ebayes(fit,...)
+			coef <- 1
+		}
 	}
 
-#	Check for lods compoent
+#	Check for lods component
 	if(is.null(eb$lods)) {
-		if(sort.by=="B") stop("Trying to sort.by B, but B-statistic (lods) not found in MArrayLM object",.call=FALSE)
-		if(!is.null(resort.by)) if(resort.by=="B") stop("Trying to resort.by B, but B-statistic (lods) not found in MArrayLM object",.call=FALSE)
+		if(sort.by=="B") stop("Trying to sort.by B, but B-statistic (lods) not found in MArrayLM object",call.=FALSE)
+		if(!is.null(resort.by)) if(resort.by=="B") stop("Trying to resort.by B, but B-statistic (lods) not found in MArrayLM object",call.=FALSE)
+		include.B <- FALSE
+	} else {
+		include.B <- TRUE
+	}
+
+#	Extract statistics for table
+	M <- fit$coefficients[,coef]
+	tstat <- as.matrix(eb$t)[,coef]
+	P.Value <- as.matrix(eb$p.value)[,coef]
+	if(include.B) B <- as.matrix(eb$lods)[,coef]
+
+#	Apply multiple testing adjustment
+	adj.P.Value <- p.adjust(P.Value,method=adjust.method)
+
+#	Thin out fit by p.value and lfc thresholds	
+	if(p.value < 1 | lfc > 0) {
+		sig <- (adj.P.Value <= p.value) & (abs(M) >= lfc)
+		if(any(is.na(sig))) sig[is.na(sig)] <- FALSE
+		if(all(!sig)) return(data.frame())
+		genelist <- genelist[sig,,drop=FALSE]
+		M <- M[sig]
+		A <- A[sig]
+		tstat <- tstat[sig]
+		P.Value <- P.Value[sig]
+		adj.P.Value <- adj.P.Value[sig]
+		if(include.B) B <- B[sig]
+		rn <- rn[sig]
+	}
+
+#	Are enough rows left?
+	if(length(M) < number) number <- length(M)
+	if(number < 1) return(data.frame())
+
+#	Select top rows
+	ord <- switch(sort.by,
+		logFC=order(abs(M),decreasing=TRUE),
+		AveExpr=order(A,decreasing=TRUE),
+		P=order(P.Value,decreasing=FALSE),
+		t=order(abs(tstat),decreasing=TRUE),
+		B=order(B,decreasing=TRUE),
+		none=1:length(M)
+	)
+	top <- ord[1:number]
+
+#	Assemble output data.frame
+	if(is.null(genelist))
+		tab <- data.frame(logFC=M[top])
+	else {
+		tab <- data.frame(genelist[top,,drop=FALSE],logFC=M[top],stringsAsFactors=FALSE)
+	}
+	if(confint) {
+		if(is.numeric(confint)) alpha <- (1+confint[1])/2 else alpha <- 0.975
+		margin.error <- sqrt(eb$s2.post[top])*fit$stdev.unscaled[top,coef]*qt(alpha,df=eb$df.total[top])
+		tab$CI.L <- M[top]-margin.error
+		tab$CI.R <- M[top]+margin.error
+	}
+	if(!is.null(A)) tab$AveExpr <- A[top]
+	tab <- data.frame(tab,t=tstat[top],P.Value=P.Value[top],adj.P.Val=adj.P.Value[top])
+	if(include.B) tab$B <- B[top]
+	rownames(tab) <- rn[top]
+
+#	Resort table
+	if(!is.null(resort.by)) {
+		ord <- switch(resort.by,
+			logFC=order(tab$logFC,decreasing=TRUE),
+			AveExpr=order(tab$AveExpr,decreasing=TRUE),
+			P=order(tab$P.Value,decreasing=FALSE),
+			t=order(tab$t,decreasing=TRUE),
+			B=order(tab$B,decreasing=TRUE)
+		)
+		tab <- tab[ord,]
+	}
+
+	tab
+}
+
+toptable <- function(fit,coef=1,number=10,genelist=NULL,A=NULL,eb=NULL,adjust.method="BH",sort.by="B",resort.by=NULL,p.value=1,lfc=0,confint=FALSE,...)
+#	Summary table of top genes for a single coefficient
+#	Old non-object-orientated version, marked as deprecated from 1 Feb 2018.
+#	Gordon Smyth
+#	21 Nov 2002. Last revised 1 Feb 2018.
+{
+	if(is(fit,"MArrayLM")) {
+		message("toptable() is deprecated and will be removed in the future version of limma. Please use topTable() instead.")
+	}
+
+#	Check fit
+	fit <- unclass(fit)
+	fit$coefficients <- as.matrix(fit$coefficients)
+	rn <- rownames(fit$coefficients)
+
+#	Check coef is length 1
+	if(length(coef)>1) {
+		coef <- coef[1]
+		warning("Treat is for single coefficients: only first value of coef being used")
+	}
+
+#	Ensure genelist is a data.frame
+	if(!is.null(genelist) && is.null(dim(genelist))) genelist <- data.frame(ID=genelist,stringsAsFactors=FALSE)
+
+#	Check rownames
+	if(is.null(rn))
+		rn <- 1:nrow(fit$coefficients)
+	else
+		if(anyDuplicated(rn)) {
+			if(is.null(genelist))
+				genelist <- data.frame(ID=rn,stringsAsFactors=FALSE)
+			else
+				if("ID" %in% names(genelist))
+					genelist$ID0 <- rn
+				else
+					genelist$ID <- rn
+			rn <- 1:nrow(fit$coefficients)
+		}
+
+#	Check sort.by
+	sort.by <- match.arg(sort.by,c("logFC","M","A","Amean","AveExpr","P","p","T","t","B","none"))
+	if(sort.by=="M") sort.by="logFC"
+	if(sort.by=="A" || sort.by=="Amean") sort.by <- "AveExpr"
+	if(sort.by=="T") sort.by <- "t"
+	if(sort.by=="p") sort.by <- "P"
+
+#	Check resort.by
+	if(!is.null(resort.by)) {
+		resort.by <- match.arg(resort.by,c("logFC","M","A","Amean","AveExpr","P","p","T","t","B"))
+		if(resort.by=="M") resort.by <- "logFC"
+		if(resort.by=="A" || resort.by=="Amean") resort.by <- "AveExpr"
+		if(resort.by=="p") resort.by <- "P"
+		if(resort.by=="T") resort.by <- "t"
+	}
+
+#	Check A
+	if(is.null(A)) {
+		if(sort.by=="A") stop("Cannot sort by A-values as these have not been given")
+	} else {
+		if(NCOL(A)>1) A <- rowMeans(A,na.rm=TRUE)
+	}
+
+#	If eb is NULL, look for components in fit
+	if(is.null(eb)) {
+		ebcols <- c("t","p.value","lods")
+		if(confint) ebcols <- c("s2.post","df.total",ebcols)
+		if(all(ebcols %in% names(fit))) eb <- fit[ebcols]
+#		If still NULL, compute using ebayes just for the one column required
+		if(is.null(eb)) {
+			message("Argument 'eb' not provided, so running ebayes() to get moderated t-statistics and p-values.")
+			fit$coefficients <- fit$coefficients[,coef,drop=FALSE]
+			fit$stdev.unscaled <- as.matrix(fit$stdev.unscaled)[,coef,drop=FALSE]
+			eb <- ebayes(fit,...)
+			coef <- 1
+		}
+	}
+
+#	Check for lods component
+	if(is.null(eb$lods)) {
+		if(sort.by=="B") stop("Trying to sort.by B, but B-statistic (lods) not found in MArrayLM object")
+		if(!is.null(resort.by)) if(resort.by=="B") stop("Trying to resort.by B, but B-statistic (lods) not found in MArrayLM object",call.=FALSE)
 		include.B <- FALSE
 	} else {
 		include.B <- TRUE
