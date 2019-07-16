6bba16494e68b7041ebc1455c8308daac0a2544a
Gordon Smyth <smyth@wehi.edu.au>
2019-07-16 18:16:02 +1000

    16 July 2019: limma 3.41.7
    - Improvements to zscoreT() so that `approx=TRUE` works for NA `x`
      values and `approx=FALSE` works for very large or very small `df`
      values. A new simple robust approximation from Wallace (1959) was
      added to handle the latter two cases. Previously `approx=FALSE`
      treated any `df` greater than 10000 as infinite; this threshold is
      now raised to 1e300.

--- R/read.R
+++ R/read.R
@@ -279,7 +279,7 @@ trimWhiteSpace <- function(x)
 }
 
 protectMetachar <- function(x)
-#	Insert backslashs before an metacharacters (to allow them to be included in search strings)
+#	Insert backslashs before metacharacters (to allow them to be included in search strings)
 #	Note that backslashs themselves are not handled
 #	Gordon Smyth
 #	9 June 2004. Last modified 5 Jan 2007.
--- R/zscore.R
+++ R/zscore.R
@@ -34,24 +34,78 @@ zscoreGamma <- function(q, shape, rate = 1, scale = 1/rate)
 zscoreT <- function(x, df, approx=FALSE)
 #  Z-score equivalents for t distribution deviates
 #  Gordon Smyth
-#  24 August 2003. Last modified 3 June 2014.
+#  Created 24 August 2003. Last modified 15 July 2019.
 {
-	if(approx) {
-		if(all(df > 10000)) return(x)
+	if(length(x)==0L) return(x)
 
-#		Approximation from Hill (1970)
-		A <- df-0.5
-		B <- 48*A*A
-		W <- A*log1p(x*x/df)
-		z <- (((((-0.4*W-3.3)*W-24)*W-85.5)/(0.8*W*W+100+B)+W+3)/B+1)*sqrt(W)
-		z[x<0] <- -z[x<0]
+	if(length(df)==1L) {
+		if(is.na(df)) {
+			x[] <- NA
+			return(x)
+		}
+		if(approx) {
+			if(df > 1e300) return(x)
+			if(df > 1e5 || df < 1) {
+				z <- .zscoreTWallace(x=x,df=df)
+			} else {
+				z <- .zscoreTHill(x=x,df=df)
+			}
+		} else {
+			z <- x
+			pos <- (x>0) & is.finite(x)
+			neg <- (x<0) & is.finite(x)
+			z[pos] <- qnorm(pt(x[pos],df=df,lower.tail=FALSE,log.p=TRUE),lower.tail=FALSE,log.p=TRUE) 
+			z[neg] <- qnorm(pt(x[neg],df=df,lower.tail=TRUE,log.p=TRUE),lower.tail=TRUE,log.p=TRUE)
+		}
+		return(z)
+	}
+
+	if(length(df) != length(x)) stop("length of df doesn't match length of x")
+	if(approx) {
+		if(anyNA(df)) {
+			i <- is.na(df)
+			x[i] <- NA
+			df[i] <- Inf
+		}
+		z <- x
+		VBig <- (df > 1e300)
+		Extreme <- ((df > 1e5) & !VBig) | (df < 1)
+		Mid <- !(Big | VBig) 
+		z[Extreme] <- .zscoreTWallace(x=x[Extreme],df=df[Extreme])
+		z[Mid] <- .zscoreTHill(x=x[Mid],df=df[Mid])
 	} else {
 		z <- x
-		df <- rep(df,length.out=length(x))
-		pos <- x>0
-		if(any(pos)) z[pos] <- qnorm(pt(x[pos],df=df[pos],lower.tail=FALSE,log.p=TRUE),lower.tail=FALSE,log.p=TRUE) 
-		if(any(!pos)) z[!pos] <- qnorm(pt(x[!pos],df=df[!pos],lower.tail=TRUE,log.p=TRUE),lower.tail=TRUE,log.p=TRUE)
+		pos <- (x>0) & is.finite(x)
+		neg <- (x<0) & is.finite(x)
+		z[pos] <- qnorm(pt(x[pos],df=df[pos],lower.tail=FALSE,log.p=TRUE),lower.tail=FALSE,log.p=TRUE) 
+		z[neg] <- qnorm(pt(x[neg],df=df[neg],lower.tail=TRUE,log.p=TRUE),lower.tail=TRUE,log.p=TRUE)
 	}
+	z
+}
+
+.zscoreTWallace <- function(x, df)
+#  Z-score equivalents for t distribution deviates using an upper bound from Wallace (1959).
+#  Wallace, D. L. (1959). Bounds on normal approximations to Student's and the chi-square distributions. The Annals of Mathematical Statistics, 30(4), 1121-1130.
+#  Gordon Smyth
+#  Created 16 July 2019.
+{
+	z <- sqrt(df*log1p(x/df*x))
+	z[x<0] <- -z[x<0]
+	z
+}
+
+.zscoreTHill <- function(x, df)
+#  Z-score equivalents for t distribution deviates using Hill's 1970 approximation:
+#  Hill, G. W. (1970). Algorithm 396: Student's t-quantiles. Communications of the ACM, 13(10), 619-620.
+#  Requires df > 0.5 and is best for large df.
+#  Gordon Smyth
+#  Created 3 June 2014. Last modified 15 July 2019.
+{
+	A <- df-0.5
+	B <- 48*A*A
+	z <- A*log1p(x*x/df)
+	z <- (((((-0.4*z-3.3)*z-24)*z-85.5)/(0.8*z*z+100+B)+z+3)/B+1)*sqrt(z)
+	z[x<0] <- -z[x<0]
 	z
 }
 
