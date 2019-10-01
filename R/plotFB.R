b10624f2acad577605df33842f87d769c314dc11
Gordon Smyth <smyth@wehi.edu.au>
2019-10-01 18:43:34 +1000

    limma 3.41.17
    - New arguments `xlab`, `ylab` and `main` for plotFB(). The default
      plot title now uses the column names of the data object.

--- R/plotFB.R
+++ R/plotFB.R
@@ -1,6 +1,6 @@
 plotFB <- function(x,...) UseMethod("plotFB")
 
-plotFB.RGList <- function(x, array=1, lim="separate", pch=16, cex=0.2, ...)
+plotFB.RGList <- function(x, array=1, lim="separate", pch=16, cex=0.3, xlab="log2 Background", ylab="log2 Foreground", main=colnames(x)[array], ...)
 #	Foreground-background plot
 #	Gordon Smyth
 #	5 March 2006.  Last modified 5 April 2006.
@@ -19,22 +19,22 @@ plotFB.RGList <- function(x, array=1, lim="separate", pch=16, cex=0.2, ...)
 		lim1 <- range(c(g1,r1))
 		lim2 <- range(c(g2,r2))
 	}
-	plot(g1,g2,xlim=lim1,ylim=lim2,pch=pch,cex=cex,xlab="log2 Background",ylab="log2 Foreground",main=paste("Array",array,"Green"),...)
+	plot(g1,g2,xlim=lim1,ylim=lim2,pch=pch,cex=cex,xlab=xlab,ylab=ylab,main=paste(main,"Green"),...)
 	abline(0,1,col="blue")
-	plot(r1,r2,xlim=lim1,ylim=lim2,pch=pch,cex=cex,xlab="log2 Background",ylab="",main=paste("Array",array,"Red"),...)
+	plot(r1,r2,xlim=lim1,ylim=lim2,pch=pch,cex=cex,xlab=xlab,ylab="",main=paste(main,"Red"),...)
 	abline(0,1,col="blue")
 	invisible()
 }
 
-plotFB.EListRaw <- function(x, array=1, pch=16, cex=0.2, ...)
+plotFB.EListRaw <- function(x, array=1, pch=16, cex=0.3, xlab="log2 Background", ylab="log2 Foreground", main=colnames(x)[array], ...)
 #	Foreground-background plot
 #	Gordon Smyth
-#	13 November 2013.  Last modified 13 November 2013.
+#	13 November 2013.  Last modified 1 October 2019.
 {
 	x <- x[,array]
 	b <- log2(x$Eb)
 	f <- log2(x$E)
-	plot(b,f,pch=pch,cex=cex,xlab="log2 Background",ylab="log2 Foreground",main=paste("Array",array),...)
+	plot(b,f,pch=pch,cex=cex,xlab=xlab,ylab=ylab,main=main,...)
 	abline(0,1,col="blue")
 	invisible()
 }
