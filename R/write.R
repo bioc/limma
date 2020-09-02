0c75a5a0044bb475c31bb4d45e74893e04ad2690
Gordon Smyth <smyth@wehi.edu.au>
2020-09-02 17:36:58 +1000

    limma 3.45.12
    - write.fit() now outputs a blank column heading for the row names
      so that the number of column names in the delimited output file
      will agree with the number of columns (similar to write.csv in the
      base package).
    
    - Add more explanation about "heirarchical" method in the details
      section of the decideTests() help page.

--- R/write.R
+++ R/write.R
@@ -3,7 +3,7 @@
 write.fit <- function(fit, results=NULL, file, digits=NULL, adjust="none", method="separate", F.adjust="none", quote=FALSE, sep="\t", row.names=TRUE, ...)
 #	Write an MArrayLM fit to a file
 #	Gordon Smyth
-#	14 Nov 2003.  Last modified 3 Dec 2017.
+#	14 Nov 2003.  Last modified 2 Sep 2020.
 {
 	if(!is(fit, "MArrayLM")) stop("fit should be an MArrayLM object")
 	if(!is.null(results) && !is(results,"TestResults")) stop("results should be a TestResults object")
@@ -50,12 +50,31 @@ write.fit <- function(fit, results=NULL, file, digits=NULL, adjust="none", metho
 	tab$Genes <- fit$genes
 	tab <- data.frame(tab,check.names=FALSE)
 
+#	Unlike write.table, the row.names argument must be a logical value
+	if(is.character(row.names)) {
+		warning("attempt to set new row.names ignored. row.names argument should be TRUE or FALSE.")
+		row.names <- TRUE
+	} else {
+		if(!is.logical(row.names)) stop("row.names should be logical value")
+	}
+
+#	If row.names=TRUE but fit doesn't contain row.names, then override argument and issue warning
 	if(is.null(row.names(fit))) {
-		row.names <- FALSE
+		if(row.names) {
+			warning("fit doesn't contain row.names")
+			row.names <- FALSE
+		}
 	} else {
 		row.names(tab) <- row.names(fit)
 	}
 
-	write.table(tab,file=file,quote=quote,row.names=row.names,sep=sep,...)
+#	This function treats col.names similarly to write.csv for col.names
+#	and ensures a blank column name for the row.names column if present.
+    Call <- match.call(expand.dots = TRUE)
+    if (!is.null(Call[["col.names"]])) warning("attempt to set 'col.names' ignored")
+    if (!is.null(Call[["qmethod"]])) warning("attempt to set 'qmethod' ignored")
+	if(row.names) col.names <- NA else col.names=TRUE
+
+	write.table(tab,file=file,quote=quote,row.names=row.names,col.names=col.names,sep=sep,qmethod="double",...)
 }
 
