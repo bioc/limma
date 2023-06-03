24e6daf2ce1130ee72d01ae442acedbf74da545f
Gordon Smyth <smyth@wehi.edu.au>
2023-06-03 20:03:16 +1000

    limma 3.57.2
    - Add new argument 'group' for removeBatchEffect(). Revise help page
      example to use the new argument.

--- R/removeBatchEffect.R
+++ R/removeBatchEffect.R
@@ -3,11 +3,12 @@
 #  A refinement would be to empirical Bayes shrink
 #  the batch effects before subtracting them.
 
-removeBatchEffect <- function(x,batch=NULL,batch2=NULL,covariates=NULL,design=matrix(1,ncol(x),1),...)
+removeBatchEffect <- function(x,batch=NULL,batch2=NULL,covariates=NULL,design=matrix(1,ncol(x),1),group=NULL,...)
 #  Remove batch effects from matrix of expression data
 #  Gordon Smyth and Carolyn de Graaf
-#  Created 1 Aug 2008. Last revised 1 June 2014.
+#  Created 1 Aug 2008. Last revised 3 June 2023.
 {
+#	Covariates to remove (batch effects)
 	if(is.null(batch) && is.null(batch2) && is.null(covariates)) return(as.matrix(x))
 	if(!is.null(batch)) {
 		batch <- as.factor(batch)
@@ -21,8 +22,19 @@ removeBatchEffect <- function(x,batch=NULL,batch2=NULL,covariates=NULL,design=ma
 	}
 	if(!is.null(covariates)) covariates <- as.matrix(covariates)
 	X.batch <- cbind(batch,batch2,covariates)
+
+#	Covariates to keep (experimental conditions)
+	if(!is.null(group)) {
+		group <- as.factor(group)
+		design <- model.matrix(~group)
+	}
+
+#	Fit combined linear model
+	x <- as.matrix(x)
 	fit <- lmFit(x,cbind(design,X.batch),...)
+
+#	Subtract batch effects adjusted for experimental conditions
 	beta <- fit$coefficients[,-(1:ncol(design)),drop=FALSE]
 	beta[is.na(beta)] <- 0
-	as.matrix(x) - beta %*% t(X.batch)
+	x - beta %*% t(X.batch)
 }
