bbd90ea4abc049a00edab3652969d0348b802391
Gordon Smyth <smyth@wehi.edu.au>
2025-10-01 16:43:28 +1000

    limma 3.65.5
    - Bug fix to contrastAsCoef() when the `contrast` matrix is not of
      full column rank.

--- R/contrastAsCoef.R
+++ R/contrastAsCoef.R
@@ -1,7 +1,7 @@
 contrastAsCoef <- function(design, contrast=NULL, first=TRUE)
 #	Reform a design matrix so that one or more contrasts become simple coefficients
 #	Gordon Smyth
-#	31 August 2013. Last modified 14 Dec 2016.
+#	31 August 2013. Last modified 1 Oct 2025.
 {
 	design <- as.matrix(design)
 	if(is.null(contrast)) return(design)
@@ -9,15 +9,14 @@ contrastAsCoef <- function(design, contrast=NULL, first=TRUE)
 	if(ncol(design) != nrow(contrast)) stop("Length of contrast doesn't match ncol(design)")
 	QRc <- qr(contrast)
 	ncontrasts <- QRc$rank
-	if(ncontrasts==0) stop("contrast is all zero")
+	if(identical(ncontrasts,0L)) stop("contrast is all zero")
 	coef <- 1:ncontrasts
 	designT <- qr.qty(QRc,t(design))
-	R <- QRc$qr[coef,]
-	designT[coef,] <- backsolve(R,designT[coef,,drop=FALSE])
+	designT[coef,] <- backsolve(QRc$qr,designT,k=ncontrasts)
 	design <- t(designT)
-	colnames(design) <- paste("Q",1:ncol(design),sep="")
+	colnames(design) <- paste0("Q",1:ncol(design))
 	cn <- colnames(contrast)
-	if(is.null(cn)) cn <- paste("C",QRc$pivot[coef],sep="")
+	if(is.null(cn)) cn <- paste0("C",QRc$pivot[coef])
 	colnames(design)[coef] <- cn
 	if(!first) {
 		design <- cbind(design[,-coef,drop=FALSE],design[,coef,drop=FALSE])
